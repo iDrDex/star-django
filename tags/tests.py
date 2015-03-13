441ca00ea13679f1653f8f9061ee0e565a5babde
Alexander Schepanovski <suor.web@gmail.com>
2015-03-13 14:05:47 +0700

    Setup checks

--- tags/tests.py
+++ tags/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import lxml.html
+import pytest
 
-# Create your tests here.
+
+@pytest.fixture(autouse=True)
+def real_db(_django_cursor_wrapper):
+    _django_cursor_wrapper.enable()
+
+
+def test_search(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+    response = client.get('/?q=diabetes')
+    assert response.status_code == 200
+
+    dom = lxml.html.fromstring(response.content)
+    assert len(dom.cssselect('#data-table tr')) == 11
