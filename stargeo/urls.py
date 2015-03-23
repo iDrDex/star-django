1cb04efb7fc08ea643ab07aed32002fec07bc197
Alexander Schepanovski <suor.web@gmail.com>
2015-03-23 17:14:20 +0700

    Wrap everything into Homer theme

--- stargeo/urls.py
+++ stargeo/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('',  # noqa
-    url(r'^$', 'tags.views.search', name='search'),
+    url(r'^$', TemplateView.as_view(template_name='dashboard.j2'), name='search'),
+    url(r'^search/$', 'tags.views.search', name='search'),
     url(r'^annotate/$', 'tags.views.annotate', name='annotate'),
     url(r'^validate/$', 'tags.views.validate', name='validate'),
 
