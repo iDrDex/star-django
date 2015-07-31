50512d295cf49b38aba4bda99100f547a91b7f35
Alexander Schepanovski <suor.web@gmail.com>
2015-07-31 18:26:15 +0700

    Fix tag exclusion lost in pagination

--- tags/templatetags/handy.py
+++ tags/templatetags/handy.py
@@ -1,5 +1,6 @@
 import json
 
+import jinja2
 from django_jinja import library
 from django.db.models.query import QuerySet
 
@@ -9,3 +10,12 @@ def _json(value):
     if isinstance(value, QuerySet):
         value = list(value)
     return json.dumps(value)
+
+
+@library.global_function
+@jinja2.contextfunction
+def replace_get(context, **kwargs):
+    request = context.get('request')
+    query = request.GET.copy()
+    query.update(kwargs)
+    return '%s?%s' % (request.path, query.urlencode())
