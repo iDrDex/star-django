a350120cf3eee368f6111a5077bd5031b06c8ba4
Alexander Schepanovski <suor.web@gmail.com>
2015-08-21 00:30:16 +0700

    Refine redis logging in analysis

--- analysis/tasks.py
+++ analysis/tasks.py
@@ -7,8 +7,9 @@ from .analysis import perform_analysis
 
 @shared_task
 def analysis_task(analysis_id):
-    handler = RedisHandler(key='analysis:%d:log' % analysis_id)
-    with extra_logging_handler('analysis', handler):
+    log_key = 'analysis:%d:log' % analysis_id
+    redis_client.delete(log_key)
+    with extra_logging_handler('', RedisHandler(key=log_key)):
         analysis = Analysis.objects.get(pk=analysis_id)
         perform_analysis(analysis)
 
