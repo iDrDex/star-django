237c565d42a242ae912a477cb146ce7ad1e55fc0
Alexander Schepanovski <suor.web@gmail.com>
2015-08-21 18:08:09 +0700

    Add django-extensions

--- stargeo/settings.py
+++ stargeo/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = (
     'analysis',
 )
 if DEBUG:
-    INSTALLED_APPS += ('debug_toolbar',)
+    INSTALLED_APPS += ('debug_toolbar', 'django_extensions')
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
