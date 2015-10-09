922aa33c024c2a369b073b3f2ccbaceae444ff09
Alexander Schepanovski <suor.web@gmail.com>
2015-10-09 17:53:35 +0700

    Allow long usernames

--- stargeo/__init__.py
+++ stargeo/__init__.py
@@ -2,6 +2,7 @@ import os
 
 if os.environ.get('DJANGO_SETTINGS_MODULE'):
     from django.contrib.auth.models import User, Group, Permission
+    User._meta.get_field('username').max_length = 127
     User._meta.db_table = 'dauth_user'
     Group._meta.db_table = 'dauth_group'
     Permission._meta.db_table = 'dauth_permission'
--- stargeo/auth_migrations/0001_initial.py
+++ stargeo/auth_migrations/0001_initial.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
                 ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
                 ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
-                ('username', models.CharField(help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', unique=True, max_length=30, verbose_name='username', validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username.', 'invalid')])),
+                ('username', models.CharField(help_text='Required. Letters, digits and @/./+/-/_ only.', unique=True, max_length=127, verbose_name='username', validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username.', 'invalid')])),
                 ('first_name', models.CharField(max_length=30, verbose_name='first name', blank=True)),
                 ('last_name', models.CharField(max_length=30, verbose_name='last name', blank=True)),
                 ('email', models.EmailField(max_length=75, verbose_name='email address', blank=True)),
