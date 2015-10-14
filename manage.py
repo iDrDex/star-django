a0b0458cd0d32d38b931668e737e994c4bd80590
Alexander Schepanovski <suor.web@gmail.com>
2015-10-14 16:06:52 +0700

    Move env load before monkey patches

--- manage.py
+++ manage.py
@@ -1,20 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
-import honcho.environ
-
-
-def load_env(filename):
-    manage_dir = os.path.dirname(__file__)
-    env_file = os.path.join(manage_dir, filename)
-
-    env = honcho.environ.parse(open(env_file).read())
-    for key, value in env.items():
-        os.environ.setdefault(key, value)
 
 
 if __name__ == "__main__":
-    load_env('.env')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stargeo.settings")
 
     from django.core.management import execute_from_command_line
--- stargeo/__init__.py
+++ stargeo/__init__.py
@@ -1,4 +1,19 @@
 import os
+import honcho.environ
+
+
+def load_env(filename):
+    manage_dir = os.path.dirname(__file__)
+    env_file = os.path.join(manage_dir, filename)
+
+    env = honcho.environ.parse(open(env_file).read())
+    for key, value in env.items():
+        os.environ.setdefault(key, value)
+
+
+if 'SECRET_KEY' not in os.environ:
+    load_env('../.env')
+
 
 if os.environ.get('DJANGO_SETTINGS_MODULE'):
     from django.contrib.auth.models import User, Group, Permission
