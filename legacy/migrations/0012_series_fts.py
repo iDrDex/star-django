8cd6566534dae715cc71f1b960ccdac413154e1f
Alexander Schepanovski <suor.web@gmail.com>
2016-07-19 17:49:11 +0700

    Add FTS on series
    Using series_view in real code for now,
    this needs to be filled in first.

---
+++ legacy/migrations/0012_series_fts.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('legacy', '0011_store_attrs_in_json'),
+    ]
+
+    operations = [
+        migrations.RunSQL("""
+alter table series add column tsv tsvector;
+
+create or replace function make_attrs_tsv() returns trigger as $$
+declare
+  tmp text;
+begin
+  NEW.tsv = to_tsvector((select string_agg(value, ' ') from json_each_text(NEW.attrs::JSON)));
+  return NEW;
+end;
+$$ language plpgsql stable;
+
+create trigger update_tsv before insert or update on series
+  for each row execute procedure make_attrs_tsv();
+
+create index series_search_idx on series using gist(tsv);
+        """,
+
+        reverse_sql="""
+drop index series_search_idx;
+drop function make_attrs_tsv() cascade;
+alter table series drop column tsv;
+        """)
+    ]
