79e53e44bab502ab4ad04e8087daa76f06f2b243
Alexander Schepanovski <suor.web@gmail.com>
2016-11-23 11:42:18 +0700

    Add forest plots draft

--- copy_static.sh
+++ copy_static.sh
@@ -4,3 +4,4 @@ cp bower_components/jquery/dist/jquery.min.* static/
 cp bower_components/lodash/lodash.min.js static/
 cp bower_components/flot/jquery.flot.time.js static/
 cp bower_components/jquery-cookie/jquery.cookie.js static/
+cp bower_components/d3/d3.min.js static/
