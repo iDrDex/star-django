fe54312f56fbaf7e21dc0267f09b93bb74bdd5cb
Ilya Beda <ir4y.ix@gmail.com>
2016-12-07 19:22:30 +0700

    Add merge migration

---
+++ legacy/migrations/0025_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('legacy', '0024_analysis_specie'),
+        ('legacy', '0024_series_platforms'),
+    ]
+
+    operations = [
+    ]
