158851b14af74ac20e303f1346bb58005919dfb2
Ilya Beda <ir4y.ix@gmail.com>
2016-12-20 13:45:04 +0700

    Fix browser cache problem by adding versionning for static assets

--- stargeo/settings.py
+++ stargeo/settings.py
@@ -120,6 +120,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 # For debug toolbar
 DEBUG_TOOLBAR_CONFIG = {
     'JQUERY_URL': '/static/jquery.min.js'
