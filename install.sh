fc0d8e026ca4266b8151d1268677ef621df3ae12
Alexander Schepanovski <suor.web@gmail.com>
2017-06-02 14:51:41 +0700

    Remove some unused files

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install -y nginx uwsgi-emperor redis-server rabbitmq-server supervisor
-
-sudo cp stuff/celery.conf /etc/supervisor/conf.d/
-sudo supervisorctl reload
--- lint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-flake8 .
-flake8 --ignore=N,E302 --exclude= analysis/analysis.py
