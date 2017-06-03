694acc40acdc79cf8cb4b3cc90ff6e5806945ff4
Alexander Schepanovski <suor.web@gmail.com>
2017-06-04 00:29:28 +0700

    Use GIN index on series

---
+++ legacy/migrations/0027_use_gin_on_series.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('legacy', '0026_auto_20161207_1257'),
+    ]
+
+    operations = [
+        migrations.RunSQL("""
+drop index series_search_idx;
+create index series_search_gin_idx on series using gin(tsv);
+        """,
+
+        reverse_sql="""
+drop index series_search_gin_idx;
+create index series_search_idx on series using gist(tsv);
+        """)
+    ]
