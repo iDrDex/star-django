df76f5aac09d31099f52fdbe736393ac571be714
Alexander Schepanovski <suor.web@gmail.com>
2017-06-05 16:02:59 +0700

    Lock everything out for unregistered users

---
+++ core/middleware.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.decorators import login_required
+
+
+class LoginRequired:
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        if request.path == '/accounts/login/' or request.path.startswith('/admin/'):
+            return None
+        return login_required(view_func)(request, *view_args, **view_kwargs)
