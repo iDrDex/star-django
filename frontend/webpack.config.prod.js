08b911b28b0b8b0f459f42db1435f073bf950daf
Ilya Beda <ir4y.ix@gmail.com>
2017-06-23 17:39:59 +0700

    Put release js bundle to dist

--- frontend/webpack.config.prod.js
+++ frontend/webpack.config.prod.js
@@ -4,6 +4,12 @@ var path = require('path');
 
 module.exports = Object.assign(require('./webpack.config.js'), {
     devtool: 'cheap-source-map',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        library: ['App', '[name]'],
+        libraryTarget: 'window',
+    },
     plugins: [
         new webpack.NoErrorsPlugin(),
         new webpack.optimize.UglifyJsPlugin({
