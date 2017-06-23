5fd20191ddb694fc38d34468a505384aa132190b
Ilya Beda <ir4y.ix@gmail.com>
2017-06-23 17:33:01 +0700

    Fix frontent production build

--- frontend/webpack.config.prod.js
+++ frontend/webpack.config.prod.js
@@ -4,10 +4,6 @@ var path = require('path');
 
 module.exports = Object.assign(require('./webpack.config.js'), {
     devtool: 'cheap-source-map',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
     plugins: [
         new webpack.NoErrorsPlugin(),
         new webpack.optimize.UglifyJsPlugin({
