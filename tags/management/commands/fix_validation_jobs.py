9acab6ff54f38152eaf5bdab040eb9610019666c
Alexander Schepanovski <suor.web@gmail.com>
2017-07-24 18:41:00 +0700

    Update fix validation jobs script and drop old parts

---
+++ tags/management/commands/fix_validation_jobs.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from tqdm import tqdm
+
+from tags.models import SeriesAnnotation, ValidationJob
+
+
+class Command(BaseCommand):
+    help = 'Fixes validation jobs'
+
+    def handle(self, **options):
+        has_jobs = set(ValidationJob.objects.values_list('annotation_id', flat=True))
+        annos = SeriesAnnotation.objects.values_list('pk', 'best_cohens_kappa')
+        missing = [pk for pk, k in annos if k != 1 and pk not in has_jobs]
+
+        if missing:
+            print('> Going to create %d validation jobs...' % len(missing))
+            for pk in tqdm(missing):
+                ValidationJob.objects.create(annotation_id=pk)
