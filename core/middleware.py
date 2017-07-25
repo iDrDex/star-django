63bdbef02759372d97a7873707d481c2933f5a74
Alexander Schepanovski <suor.web@gmail.com>
2017-07-25 20:27:06 +0700

    Allow password reset

--- core/middleware.py
+++ core/middleware.py
@@ -3,6 +3,6 @@ from django.contrib.auth.decorators import login_required
 
 class LoginRequired:
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if request.path == '/accounts/login/' or request.path.startswith('/admin/'):
+        if request.path.startswith('/accounts/') or request.path.startswith('/admin/'):
             return None
         return login_required(view_func)(request, *view_args, **view_kwargs)
