4b0d746947b0a3c0f732c1c8609ab0f5a1cbab8a
Alexander Schepanovski <suor.web@gmail.com>
2017-08-21 18:54:15 +0700

    Supplement djapi principles

--- djapi.py
+++ djapi.py
@@ -1,6 +1,7 @@
 """
 Principles:
 - library not framework (helpers + decorators)
+- avoid abstractions, especially IOC
 - reuse django (i.e. validation)
     - when impossible to reuse, mimic it
 - do not hide intentions
