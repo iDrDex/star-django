af2dfc652dd46e0e820286691ee9dcc8f0ff29df
Alexander Schepanovski <suor.web@gmail.com>
2017-10-26 16:49:14 +0700

    Fix SeriesAnnotations.is_active default

---
+++ tags/migrations/0031_series_annos_active_by_default.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tags', '0030_rawseriesannotation_obsolete'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='seriesannotation',
+            name='is_active',
+            field=models.BooleanField(default=True),
+        ),
+    ]
